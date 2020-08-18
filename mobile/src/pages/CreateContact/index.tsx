a1e2c8a713fff077b3da205122267459c38b3391
Gustavo <gcrodrigues413@gmail.com>
2020-08-17 22:03:51 -0300

    Create contact screen

---
+++ mobile/src/pages/CreateContact/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+// import { Container } from './styles';
+
+const CreateContact: React.FC = () => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  );
+};
+
+export default CreateContact;
